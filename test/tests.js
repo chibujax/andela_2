3cf86f12fe688dc9e06c1cd50eafde87c3cff2f7
chibujax <chibujax@gmail.com>
2017-02-12 08:37:26 +0100

    Build OOP test cases and OOP functions

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,10 @@
 (function() {
   'use strict';
   var datatypeslab = require("../app/DataTypesLab/library.js");
-  //var fizbuz = require("../app/fizz_buzz/fizzbuzz.js");
+  var getPrimesLab = require('../app/getprimes/library.js');
+  var cars = require('../app/OOP/library.js');
+
+
   //var geo = require("../app/arit_geo/arit.js");
 
   describe("Data types tests ", function() {
@@ -89,7 +92,6 @@
     });
   });
 
-  var getPrimesLab = require('../app/getprimes/library.js')
 
   describe("Test cases for getPrimes function", function() {
       it("should return 'negative integers can not be prime' for negative integers", function() {
@@ -141,5 +143,63 @@
       });
     
   });
+
+  describe("Car Class: Create a car, make it drive", function() {
+
+    it("The car should be a type of `object`, and an instance of the `Car` class", function() {
+      var honda = new cars.Car('Honda');
+      expect(typeof honda).toEqual(typeof {});
+      expect(honda instanceof cars.Car).toBeTruthy();
+    });
+
+    it("The car should be called 'General' if no name is passed as a parameter", function() {
+      var gm = new cars.Car();
+      expect(gm.name).toEqual('General');
+      expect(gm.model).toBe('GM');
+    });
+
+    it("The car name and model should be a property of the car", function() {
+      var toyota  = new cars.Car('Toyota', 'Corolla');
+      expect(toyota.name).toBe('Toyota');
+      expect(toyota.model).toBe('Corolla');
+    });
+
+    it("The car shoud have four (4) doors except its a Porshe or Koenigsegg", function() {
+      var opel  = new cars.Car('Opel', 'Omega 3');
+      expect(opel.numOfDoors).toBe(4);
+
+      var porshe = new cars.Car('Porshe', '911 Turbo');
+      expect(porshe.numOfDoors).toBe(2);
+      porshe.drive(5);
+      expect(porshe.speed).toBe('250 km/h');
+      expect((function(){return new cars.Car('Koenigsegg', 'Agera R');}()).numOfDoors).toBe(2);
+    });
+
+    it("The car shoud have four (4) wheels except its a type of trailer", function() {
+      var man  = new cars.Car('MAN', 'Truck', 'trailer');
+      expect(man.numOfWheels).toBe(8);
+      expect(man.isSaloon).toBe(false);
+
+      var koenigsegg = new cars.Car('Koenigsegg', 'Agera R');
+      expect(koenigsegg.numOfWheels).toBe(4);
+      expect(koenigsegg.isSaloon).toBeTruthy();
+    });
+
+    it("The Trailer should have speed 0 km/h until you put `the pedal to the metal`", function() {
+      var man  = new cars.Car('MAN', 'Truck', 'trailer');
+      expect(man.speed).toBe('0 km/h');
+      man.drive(7);
+      expect(man.speed).toBe('77 km/h');
+    });
+
+    it("The car drive function should return the instance of the Car class", function() {
+      var man  = new cars.Car('MAN', 'Truck', 'trailer');
+      var drivingMan = man.drive(7);
+      expect(drivingMan instanceof cars.Car).toBeTruthy();
+      expect(typeof drivingMan.drive).toBe(typeof (function (){}));
+      expect(man.speed).toBe(drivingMan.speed);
+    });
+  });
+
 
 })();
