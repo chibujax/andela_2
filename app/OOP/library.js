4cdc064dac437056f1ad27b39380e3713249630f
chibujax <chibujax@gmail.com>
2017-02-13 09:42:48 +0100

    Add input validation for OOP test function

--- app/OOP/library.js
+++ app/OOP/library.js
@@ -1,17 +1,26 @@
 'use strict'
 
 module.exports = {
-Car: function (name, model, type) {
-	this.name = ((name == null) ? 'General' : name);
-	this.model = ((model == null) ? 'GM' : model);
-	this.type = type;
-	this.numOfDoors = ((name == 'Porshe' || name == 'Koenigsegg') ? 2 : 4);
-	this.numOfWheels = ((type == 'trailer') ? 8 : 4);
-	this.isSaloon = ((type == 'trailer') ? false:  true);
-	this.speed = '0 km/h';
-	this.drive = function (spd) {
-		this.speed = (type == 'trailer') ? (spd * 11) + ' km/h' : (spd * 50) + ' km/h';
-	return this;
+	Car: function (name, model, type) {
+		if (typeof(name) !== "string"){
+		    name =null;
+		}
+		if (typeof(model) !== "string"){
+		    model =null;
+		}
+		if (typeof(type) !== "string"){
+		    type =null;
+		} 	 	 
+		this.name = ((name == null) ? 'General' : name);
+		this.model = ((model == null) ? 'GM' : model);
+		this.type = type;
+		this.numOfDoors = ((name == 'Porshe' || name == 'Koenigsegg') ? 2 : 4);
+		this.numOfWheels = ((type == 'trailer') ? 8 : 4);
+		this.isSaloon = ((type == 'trailer') ? false:  true);
+		this.speed = '0 km/h';
+		this.drive = function (spd) {
+			this.speed = (type == 'trailer') ? (spd * 11) + ' km/h' : (spd * 50) + ' km/h';
+		return this;
+		}
 	}
-}
 };
