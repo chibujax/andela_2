9f41cf91e5d4a5bb97dabe8138aa700ff0aa41e3
chibujax <chibujax@gmail.com>
2017-02-19 07:59:10 +0100

    Correct indentation problem caused by notepad

--- app/OOP/library.js
+++ app/OOP/library.js
@@ -1,64 +1,64 @@
 function Telephone(){
-	this.displayMessage = "";
-	this.isPowerOn = false;	
+  this.displayMessage = "";
+  this.isPowerOn = false;	
 }
 Telephone.prototype.powerOn = function(){
-	this.displayMessage = "Device is up";
-		this.isPowerOn = true;	
+  this.displayMessage = "Device is up";
+    this.isPowerOn = true;	
 };
 Telephone.prototype.powerOff = function(){
-	this.displayMessage = "";
-		this.isPowerOn = false;
+  this.displayMessage = "";
+    this.isPowerOn = false;
 };
 Phone = function(name,model,os) {	
-	//Ensure Only string can be assigened. Not [1]
-	(typeof name == "string") ? this.name = name	: this.name = "General";
-	(typeof model == "string") ? this.model = model	: this.model = "Mobile";
-	(typeof os == "string") ? this.os = os	: this.os = "Android";
-	
-	if (this.name  === "Tecno" || this.name === "Infinix" || this.name === "General") { 	
-	  	this.CPU = "MTK Processor";		
-	  	this.isAndroid = true;			
-	  	this.doubleSim = true;			
-	  	this.isIphone = false;	
-	  	this.isCharging = false;		
-	}	
-	else {			
-		if (this.os === "iOS") {
-			this.CPU = "Apple Chipset";		
-		  	this.isAndroid = false;			
-		  	this.isIphone = true;			
-		  	this.doubleSim = false;			
-		  	this.isCharging = false;		
-		}
-		else if (this.os === "Android") {
-			this.CPU = "SnapDragon Processor";		
-		  	this.isAndroid = true;			
-		  	this.doubleSim = false;			
-		  	this.isIphone = false;			
-		  	this.isCharging = false;		
-		}
-		else {		
-			this.CPU = "Microsoft Processor/Java CPU";		
-			this.isAndroid = false;			
-		  	this.isIphone = false;			
-		  	this.doubleSim = false;			
-		  	this.isCharging = false;		
-		}
-	}
+  //Ensure Only string can be assigened. Not [1]
+  (typeof name == "string") ? this.name = name	: this.name = "General";
+  (typeof model == "string") ? this.model = model	: this.model = "Mobile";
+  (typeof os == "string") ? this.os = os	: this.os = "Android";
+  
+  if (this.name  === "Tecno" || this.name === "Infinix" || this.name === "General") { 	
+      this.CPU = "MTK Processor";		
+      this.isAndroid = true;			
+      this.doubleSim = true;			
+      this.isIphone = false;	
+      this.isCharging = false;		
+  }	
+  else {			
+    if (this.os === "iOS") {
+      this.CPU = "Apple Chipset";		
+        this.isAndroid = false;			
+        this.isIphone = true;			
+        this.doubleSim = false;			
+        this.isCharging = false;		
+    }
+    else if (this.os === "Android") {
+      this.CPU = "SnapDragon Processor";		
+        this.isAndroid = true;			
+        this.doubleSim = false;			
+        this.isIphone = false;			
+        this.isCharging = false;		
+    }
+    else {		
+      this.CPU = "Microsoft Processor/Java CPU";		
+      this.isAndroid = false;			
+        this.isIphone = false;			
+        this.doubleSim = false;			
+        this.isCharging = false;		
+    }
+  }
 }
 Phone.prototype = new Telephone();
 Phone.prototype.charge = function() {
-	this.isCharging = true;	
+  this.isCharging = true;	
 };
 Phone.prototype.displayInfo = function() {
-	return "Name: " + this.name + " Model: " + this.model + " Os: " + this.os;	
+  return "Name: " + this.name + " Model: " + this.model + " Os: " + this.os;	
 };
 Phone.prototype.stopCharge = function() {
-	this.isCharging = false;	
+  this.isCharging = false;	
 };
 Phone.prototype.powerOn = function(){
-	this.displayMessage = "Device is up, seraching for wireless signal";
-	this.isPowerOn = true;	
+  this.displayMessage = "Device is up, seraching for wireless signal";
+  this.isPowerOn = true;	
 };	
 
--- app/getprimes/library.js
+++ app/getprimes/library.js
@@ -1,52 +1,52 @@
 'use strict'
 
 module.exports = {
-	getPrimes: function (num) {
-	  var primes = [2,3];
-	  var empty_a = [];
-	  var checkPrime = function(n) {
-	    if (n < 2) return false;
-			//An integer is prime if it is not divisible by any prime less than or equal to its square root
+  getPrimes: function (num) {
+    var primes = [2,3];
+    var empty_a = [];
+    var checkPrime = function(n) {
+      if (n < 2) return false;
+      //An integer is prime if it is not divisible by any prime less than or equal to its square root
 
-	    var q = Math.floor(Math.sqrt(n));  //Faster than total loop
+      var q = Math.floor(Math.sqrt(n));  //Faster than total loop
 
-	    for (var i = 2; i <= q; i++) {
-	        if (n % i == 0)
-	        {
-	            return false;
-	        }
-	    }
+      for (var i = 2; i <= q; i++) {
+          if (n % i == 0)
+          {
+              return false;
+          }
+      }
 
-	    return true;
-		}
-		if(num == 0){
-			return "0 cannot be prime";
-		}
-		var real_num = parseInt(num)
-		if (!real_num){
-		  return 'Must be a number';
-		}
-	  
-	  if (num < 0) {
-	    return "negative integers can not be prime";
-	  }
-	  else if (num === 1) {
-	    return empty_a;  
-	  }
-	  else if (num === 2) {
-	    return [2];
-	  }
-	  else if (num === 3) {
-	    return primes;
-	  }
-	  else {
-	    for(var i = 4; i <= num; i++) {
-	          if(checkPrime(i) === true ){
-	              primes.push(i);
-	          }
-	    }
-	    return primes;
-	  }
-	}
+      return true;
+    }
+    if(num == 0){
+      return "0 cannot be prime";
+    }
+    var real_num = parseInt(num)
+    if (!real_num){
+      return 'Must be a number';
+    }
+    
+    if (num < 0) {
+      return "negative integers can not be prime";
+    }
+    else if (num === 1) {
+      return empty_a;  
+    }
+    else if (num === 2) {
+      return [2];
+    }
+    else if (num === 3) {
+      return primes;
+    }
+    else {
+      for(var i = 4; i <= num; i++) {
+            if(checkPrime(i) === true ){
+                primes.push(i);
+            }
+      }
+      return primes;
+    }
+  }
 
 }
